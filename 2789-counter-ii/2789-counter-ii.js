4226747e8bcb554ab2b8ec54032edfdf3f7c5c15
sy <sduddla921@gmail.com>
2025-08-14 17:33:21 +0900

    Time: 39 ms (92.82%), Space: 56.4 MB (62.14%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let num = init;
+    return {
+        increment: () => num += 1,
+        decrement: () => num -= 1,
+        reset: () => {
+            num = init;
+            return num;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
